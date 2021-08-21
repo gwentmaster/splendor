074951d2fa8e91bc7b2b77f1976b308901a73b67
gwentmaster <gwentmaster@vivaldi.net>
2021-08-21 16:41:21 +0800

    temp

---
+++ splendor/Card/Bank.gd
@@ -0,0 +1,43 @@
+extends Panel
+
+
+var CARD_DATA = load("res://Card/CardData.gd").new()
+var COST_ORDER = CARD_DATA.COST_ORDER
+
+
+export var deck = {
+	"senior": [],
+	"junior": [],
+	"primary": []
+}
+
+func _ready():
+	reset()
+	
+	for level in ["senior", "junior", "primary"]:
+		for slot in range(4):
+			draw_card(level, slot)
+	
+	
+func reset():
+	deck = {
+		"senior": range(20),
+		"junior": range(30),
+		"primary": range(40)
+	}
+	for v in deck.values():
+		v.shuffle()
+
+
+func draw_card(level, slot):
+	
+	# 无卡可补充
+	if len(deck[level]) == 0:
+		return null
+	
+	# 从牌堆取一张牌放入对应位置, 由于牌堆为乱序, 故直接调用"pop"方法
+	get_node(level.capitalize()).get_node("Slot" + str(slot)).set_card(level, deck[level].pop_back())
+	
+	# 若卡已抽光, 设置对应牌堆为不可选中
+	if len(deck[level]) == 0:
+		get_node(level.capitalize() + "Bank").set_selectable(false)
--- splendor/Card/Card.gd
+++ splendor/Card/Card.gd
@@ -4,7 +4,11 @@ extends Area2D
 export var score = 0
 export var color = "blue"
 export var cost = {"white": 0, "blue": 0, "green": 0, "red": 0, "brown": 0}
-var choosed = false
+export var reservable = true
+export var buyable = true
+export var selectable = true
+var selected = false
+
 
 var CARD_DATA = load("res://Card/CardData.gd").new()
 var COST_ORDER = CARD_DATA.COST_ORDER
@@ -14,6 +18,7 @@ func _ready():
 	
 	$PurchaseButton.hide()
 	$ReserveButton.hide()
+	
 
 func set_card(level, number):
 	# 设置卡牌, 加载卡图及卡牌数据
@@ -25,15 +30,14 @@ func set_card(level, number):
 	
 	# number为负, 表示此卡为卡背
 	if number < 0:
-		$Sprite.texture = load("res://Assets/image/" + level + "_card/back.jpg")
+		$CardButton/Image.texture = load("res://Assets/image/" + level + "_card/back.jpg")		
 		score = null
 		color = null
 		cost = null
 		return self
 	
 	# 正常发展卡
-	$Sprite.texture = load("res://Assets/image/" + level + "_card/" + str(number) + ".jpg")
-	$Sprite.scale = $Sprite.scale * self.scale
+	$CardButton/Image.texture = load("res://Assets/image/" + level + "_card/" + str(number) + ".jpg")	
 	
 	var data = {
 		"primary": CARD_DATA.PRIMARY_CARD_DATA,
@@ -46,19 +50,22 @@ func set_card(level, number):
 		cost[COST_ORDER[i]] = data["cost"][i]
 	
 	return self
-
-
-func _on_CardButton_pressed():
-	# 点击卡牌时切换"购买"与"保留"两个按钮的显示状态
 	
-	if choosed == true:
-		choosed = false
+	
+func set_unselected():
+	if selected:
+		selected = false
 		$PurchaseButton.hide()
 		$ReserveButton.hide()
-	else:
-		choosed = true
-		$PurchaseButton.show()
-		$ReserveButton.show()
+		
+		
+func set_selectable(flag):
+	if flag and not selectable:
+		selectable = true
+	elif selectable and not flag:
+		selectable = false
+		if selected:
+			set_unselected()
 
 
 func show_gold(flag):
@@ -68,3 +75,21 @@ func show_gold(flag):
 			$ReserveButton/Gem.show()
 		false:
 			$ReserveButton/Gem.hide()
+
+
+func _on_CardButton_gui_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if not selectable:
+			return null
+		if selected == true:
+			selected = false
+			$PurchaseButton.hide()
+			$ReserveButton.hide()
+		else:
+			get_tree().call_group_flags(2, "cards", "set_unselected")
+
+			selected = true
+			if buyable:
+				$PurchaseButton.show()
+			if reservable:
+				$ReserveButton.show()
