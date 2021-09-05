7d1d2b7f42cdf2905de8df8641a165221b31f6d3
gwentmaster <gwentmaster@vivaldi.net>
2021-09-05 20:21:11 +0800

    手牌区ui重构

--- splendor/Player/Hand.gd
+++ splendor/Player/Hand.gd
@@ -82,7 +82,8 @@ func refresh_gem_num() -> void:
 	gem_sum = 0
 	for key in gem_num.keys():
 		gem_sum += gem_num[key]
-		$Resource.get_node(key.capitalize()).get_node("GemCounter").text = str(gem_num[key])
+		$Resource.get_node(key.capitalize() + "/GemCounter").text = str(gem_num[key])
+	$Resource/GemSum.text = str(gem_sum) + "/10"
 	get_tree().call_group("gem_stage", "set_player_gem_num", gem_sum)
 
 
